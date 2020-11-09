26a5c4b9a2ea991cf989a01ff4b4e5eaa85f9dd2
Yurarara-ed <st091607@gmail.com>
2020-11-09 23:45:28 +0500

    issue-1 Turn on a commit-msg hook and copy cod from this hook to a task3.sh

--- task3.sh
+++ task3.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+
+commitRegex='^(issue-[0-9]* [A-Za-z0-9+])'
+if ! grep -qE "$commitRegex" "$1"; then
+    echo "Aborting according commit message policy. Please specify JIRA issue PONY-XXXX."
+    exit 1
+fi
